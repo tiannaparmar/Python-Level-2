c73cc0ecc96058ded90404654b61ac52f3ee6a4c
tiannaparmar <68537504+tiannaparmar@users.noreply.github.com>
2020-07-22 11:29:20 +0300

    Added a functions file that gets the total of two numbers

---
+++ Functions.py
@@ -0,0 +1,18 @@
+#Create new file
+#Save as Functions.py
+#Save in Python-Level-2 folder ---- repo(repository)
+
+#Add two numbers
+
+def AddNumbers(x,y):       #Definition of the function
+
+    return x + y
+
+
+#Call our Function
+
+Total = AddNumbers(4,9)
+
+#Output the results
+
+print(f"The sum total is = {Total}")
