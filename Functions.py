a309e0f6932bdf3dc5ab35685dda4db3d99a64eb
tiannaparmar <68537504+tiannaparmar@users.noreply.github.com>
2020-07-22 11:46:12 +0300

    Requesting user to enter inputs from keyboard

--- Functions.py
+++ Functions.py
@@ -16,3 +16,17 @@ Total = AddNumbers(4,9)
 #Output the results
 
 print(f"The sum total is = {Total}")
+
+#Ask the user for inputs from the keyboard
+
+num1 = int(input("Enter any number \n"))
+
+num2 = int(input("Enter another number \n"))
+
+#Call the function once again
+
+sum = AddNumbers(num1,num2)
+
+#Print the sum 
+
+print("The sum now of {0} plus {1} is equal to {2}".format(num1, num2, sum))
